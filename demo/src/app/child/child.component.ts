86b0e6f516ca28950b8acd7b925c1c189b8300c3
cdufour <opusidea@gmail.com>
2023-06-21 16:30:42 +0200

    exo2

--- demo/src/app/child/child.component.ts
+++ demo/src/app/child/child.component.ts
@@ -10,9 +10,13 @@ export class ChildComponent {
     title: string = 'Child Component';
     age: number = 24;
     counter: number = 0;
+    fruits: string[] = ['pomme', 'poire', 'banane'];
 
     // methods
     increment() {
         this.counter += 1;
+        if (this.counter == 5) {
+            this.fruits.pop();
+        }
     }
 }
